4d5ce4c5d6fdfa82a7712a00e19934d5e7eba541
Lakshay Jatawat <96945080+2111lakshay@users.noreply.github.com>
2023-03-08 22:53:04 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.optionmenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+public class MainActivity extends AppCompatActivity {
+    TextView text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.text);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item click
+        switch (item.getItemId())
+        {
+            case R.id.notification:
+                Toast.makeText(this, "Notification", Toast.LENGTH_SHORT).show();
+                break;
+                case R.id.help:
+                    Toast.makeText(this, "Help", Toast.LENGTH_SHORT).show();
+                    break;
+                    case R.id.setting:
+                        Toast.makeText(this, "Setting", Toast.LENGTH_SHORT).show();
+                        break; case R.id.logout:
+                            Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show();
+                            break;
+                            default:
+                                break;
+        }
+        return super.onOptionsItemSelected(item); }
+}
+
